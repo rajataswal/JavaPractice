05d5f2d8c8ccfd491401a80ad792abaf27dc621e
rajat <rajat.lge.95@lge.com>
2017-08-19 17:13:57 +0530

    constructor finished

--- JavaP/Box.java
+++ JavaP/Box.java
@@ -1,5 +1,27 @@
 class Box {
-	int h;
-	int w;
-	int b;
+	double h;
+	double w;
+	double b;
+	
+	double volume(){
+		//System.out.println("volume is ");
+		return (h*w*b);
+	}
+
+//	void setdim(double height,double width,double breadth){
+//	
+//		h=height;
+//		w=width;
+//		b=breadth;
+//	}	
+	void setdim(double height,double width,double bredth){
+		h = height;
+		w = width;
+		b = bredth;
+	}
+
+	Box(){
+		h=1;w=1;b=1;
+		//System.out.println("inside constructor");
+	}
 }
--- JavaP/Boxdemo.java
+++ JavaP/Boxdemo.java
@@ -1,10 +1,32 @@
+import java.lang.Double;
 class Boxdemo{
-	public static void main(){
-		
+	public static void main(String[] args){
 		Box mybox = new Box();
-		mybox.h=100;
-
-		System.out.println("h== "+ mybox.h);
+		
+//		System.out.println("initial volume= "+mybox.volume());
+//		
+//		mybox.setdim(Double.parseDouble(args[0]),Double.parseDouble(args[1]),Double.parseDouble(args[2]));
+//		
+//		System.out.println("h== "+ mybox.volume());
+//
+//		Box a= new Box();
+//
+//		Box b;
+//		a.setdim(1,2,3);
+//		System.out.println("volumme a"+a.volume());
+//		System.out.println("setting b=a");
+//		b=a;
+//		
+//		System.out.println("volume b= "+b.volume());
+//		b.setdim(2,2,2);
+//		a.setdim(3,3,3);
+//
+//
+//		System.out.println("volume a= "+a.volume()+"vol b= "+b.volume());
+		
+		mybox.setdim(2,2,2);
+		System.out.println(mybox.volume());
+	
 	}
 
 }
