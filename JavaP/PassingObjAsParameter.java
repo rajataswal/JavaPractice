55fe453a853f37f4a63e17ac04229316eddf3982
rajat <rajat.lge.95@lge.com>
2017-08-22 19:24:54 +0530

    correct date

--- JavaP/PassingObjAsParameter.java
+++ JavaP/PassingObjAsParameter.java
@@ -1,7 +1,11 @@
 class Test{
-	int a,b;
+	int a,b;private int c;
 	Test(int a,int b){
 		this.a=a;this.b=b;
+	}
+	Test(Test obj){
+		this.a = obj.a;
+		this.b=obj.b;
 	}
 
 	boolean equalto(Test obj){
@@ -14,10 +18,11 @@ class PassingObjAsParameter{
 	public static void main(String[] args){
 		Test obj1=new Test(1,2);
 		
-		Test obj2=new Test(1,2);
+		Test obj2=new Test(obj1);//initializing obj2 using obj1
+//		obj2.c=45;	
 //		obj1=obj2;
-		if(obj1==obj2) System.out.println("true");
-		else System.out.println("false");
+//		if(obj1==obj2) System.out.println("true");
+//		else System.out.println("false");
 		
 		System.out.println(obj1.equalto(obj2));
 	}
--- JavaP/Stringclass.java
+++ JavaP/Stringclass.java
@@ -3,8 +3,8 @@ class Stringclass{
 	public static void main(String[] args){
 
 		String str="lsdf";
-		System.out.println("str = "+str.length());
+		System.out.println("str = ".length());
 		str="lkajsdflkasdflk";
-
+		
 	}
 }
